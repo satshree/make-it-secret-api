f69c6ea9125442ef23f4a25bf4986cd23860faf0
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-27 10:15:04 -0600

    setup django project

---
+++ makeitsecret/sample_local_settings.py
@@ -0,0 +1,15 @@
+# DJANGO SETTINGS
+DEBUG = True
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '5432',
+#     }
+# }
+
+ALLOWED_HOSTS = ['*']
